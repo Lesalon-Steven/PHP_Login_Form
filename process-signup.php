54d22efa6e0f463a53690ed0adf0c3c9c8ccabbf
Lesalon_Steve... <lesalonsteven@gmail.com>
2024-03-10 20:29:57 +0100

    update

--- process-signup.php
+++ process-signup.php
@@ -6,6 +6,8 @@ if (empty($_POST["name"])){
     die("Name is required");
 }
 
-    
+if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+    die("Invalid email required");
+}
 
 print_r($_POST);
